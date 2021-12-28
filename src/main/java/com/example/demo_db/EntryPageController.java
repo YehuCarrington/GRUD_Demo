81aa7f3f94ae913f9efe7565b48c6c509c110b35
Yehu <yehu41@gmail.com>
2021-12-28 06:18:49 -0400

    Added the functionality to clear the entered values

--- src/main/java/com/example/demo_db/EntryPageController.java
+++ src/main/java/com/example/demo_db/EntryPageController.java
@@ -4,13 +4,13 @@ package com.example.demo_db;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.*;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class EntryPageController extends SceneController implements Initializable {
+
 
     @FXML private ChoiceBox<String> statusChoiceBox;
     private final String[] statusChoice = { "Open", "On Hold", "Completed", "Closed" };
@@ -18,13 +18,35 @@ public class EntryPageController extends SceneController implements Initializabl
     @FXML private Button clearButton;
     @FXML private Button submitButton;
     @FXML private Button backButton;
+    @FXML private TextField projectNameTextField;
+    @FXML private TextField itemTitleTextField;
+    @FXML private TextField responsibilityTextField;
+    @FXML private TextField createdByTextField;
+    @FXML private TextArea summaryTextArea;
+    @FXML private TextArea descriptionTextArea;
+    @FXML private DatePicker createdDatePicker;
+    @FXML private DatePicker dueDatePicker;
+    @FXML private TextArea emailTextArea;
 
     @FXML
     protected void submitButtonClick(ActionEvent actionEvent) {
+        System.out.println("actionEvent = " + actionEvent);
     }
 
     @FXML
-    protected void clearButtonClick(ActionEvent actionEvent) {
+    protected void clearButtonClick() {
+        projectNameTextField.clear();
+        itemTitleTextField.clear();
+        createdByTextField.clear();
+        responsibilityTextField.clear();
+        summaryTextArea.clear();
+        emailTextArea.clear();
+        descriptionTextArea.clear();
+        createdDatePicker.getEditor().clear();
+        createdDatePicker.setValue(null);
+        dueDatePicker.getEditor().clear();
+        dueDatePicker.setValue(null);
+        statusChoiceBox.setValue("");
     }
 
     @Override
